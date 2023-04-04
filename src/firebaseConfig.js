72cc6efe6d3ab0f3f2639b15b3f944f97279710d
Gerison Kimathi <gerkim62@gmail.com>
2023-04-04 15:04:38 +0300

    update acct

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,13 +2,25 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+////the old account
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCxOxV--X-Sx-yxnak0V03hTrqzUmupl5I",
+//   authDomain: "autofill-59946.firebaseapp.com",
+//   projectId: "autofill-59946",
+//   storageBucket: "autofill-59946.appspot.com",
+//   messagingSenderId: "908136121251",
+//   appId: "1:908136121251:web:98971af113c1a5759f9016",
+// };
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCxOxV--X-Sx-yxnak0V03hTrqzUmupl5I",
-  authDomain: "autofill-59946.firebaseapp.com",
-  projectId: "autofill-59946",
-  storageBucket: "autofill-59946.appspot.com",
-  messagingSenderId: "908136121251",
-  appId: "1:908136121251:web:98971af113c1a5759f9016",
+  apiKey: "AIzaSyBUOgb4zriJ4pUaJw-MOrxpdR3qhj00w6E",
+  authDomain: "cute-autofill.firebaseapp.com",
+  projectId: "cute-autofill",
+  storageBucket: "cute-autofill.appspot.com",
+  messagingSenderId: "981516243328",
+  appId: "1:981516243328:web:f6ea62a4da136e37557165",
+  measurementId: "G-D3DCZHB7JN",
 };
 
 const app = initializeApp(firebaseConfig);
